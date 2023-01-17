a4c952c86c4a3f7af2475f1a416e556791531559
Qisguys <122888167+Qisguys@users.noreply.github.com>
2023-01-17 21:42:51 +0530

    Update in.py
    Commit

--- in.py
+++ in.py
@@ -6,7 +6,7 @@ print (name)
 
 if name == "jagga":
   print("Your chatting with 20 members only girls\nUr were went to a restaurant with a girl because mixing of birany\nAre u met her first time\nCan you please tell me atleast 10 girls name in Instagram chatting\nAnd WhatsApp also")
-
+  print("thanks you")
   
   
 
