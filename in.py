50cabac565a9113fa197ad5951290da26bc0db3f
Qisguys <122888167+Qisguys@users.noreply.github.com>
2023-01-17 21:39:43 +0530

    Update in.py
    Commit

--- in.py
+++ in.py
@@ -4,21 +4,9 @@ print("your name is")
 
 print (name)
 
-if name == jagga :
+if name == "jagga":
+  print("Your chatting with 20 members only girls\nUr were went to a restaurant with a girl because mixing of birany\nAre u met her first time\nCan you please tell me atleast 10 girls name in Instagram chatting\nAnd WhatsApp also")
 
-print ("Your chatting with 20 members only girls")
-print ("Ur were went to a restaurant with girl because mixing of birany")
-print ("Can you please tell me atleast 3girls name in Instagram chattingAre u met her first time")
-print ("And WhatsApp also")
-nan=input ("Enter first girl name")
-
-n=input ("Enter seconds girl name")
-
-na=input ("Enter third girl name")
-
-print (nan)
-  print (n)
-  print (na)
-  print ("congratulations 👏")
+  
   
 
