27ad6146bc5cda1ac81b113d4ef92341a236eecb
Qisguys <122888167+Qisguys@users.noreply.github.com>
2023-01-18 11:36:23 +0530

    Update in.py
    Commit

--- in.py
+++ in.py
@@ -6,3 +6,5 @@ print (name)
 
 if name == "jagga":
   print("Your chat with 20 members only girls\n You went to a restaurant with a girl because you mixed birany\n You met her for the first time \n Can you please tell me the names of at least 10 girls  in the chat Instagram And also WhatsApp")
+elif name == "ammu" :
+  print("Your name is boochi[ psycho pilla ]\n I am very interested to do patches of my friends \nI am walking like rabbit\nI always eat carrets\nNaku koncham thikka undi danikoka lekkundi")
