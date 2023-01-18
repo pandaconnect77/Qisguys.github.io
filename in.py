6bca07d569d2c115caf861955b76ce7a46936c2d
Qisguys <122888167+Qisguys@users.noreply.github.com>
2023-01-18 20:13:01 +0530

    Update in.py
    Commit

--- in.py
+++ in.py
@@ -10,9 +10,9 @@ elif name == "ammu":
   print("Your name is boochi[ psycho pilla ]\n I am very interested to do patches of my friends \nI am walking like rabbit\nI always eat carrets\nNaku koncham thikka undi danikoka lekkundi")
 elif name == "venkatesh":
   print("you are fall in love in recently with pace college girl \n now your are studying in qis college in branch of cse-6\n pedha kutha lodivi \n ")
-elif name == "name":
+elif name == "yeshu1":
+  print("your name is pikachu [ mental pilla ]\n Daily the patch is done by boochi \n Daily carrying a large carriage box that can eat easily 10 members \n Naku koncham thikka undhi dhaniki asalu lekkee ledhu ")
 
-Print("hello")
 elif name == "name":
    Print("hello")
 elif name == "name":
