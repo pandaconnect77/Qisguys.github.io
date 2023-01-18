24f96c6d550d054df7def1983ea8256d96d83ff7
Qisguys <122888167+Qisguys@users.noreply.github.com>
2023-01-18 21:11:45 +0530

    Update in.py
    Commit

--- in.py
+++ in.py
@@ -5,31 +5,18 @@ print("your name is")
 print (name)
 
 if name == "jagga":
-  print("Your chat with 20 members only girls\n You went to a restaurant with a girl because you mixed birany\n You met her for the first time \n Can you please tell me the names of at least 10 girls  in the chat Instagram And also WhatsApp")
+  print("Your chat with 20 members only girls\n You went to a restaurant with a girl because you mixed birany\n You met her for the first time \n Can you please tell me the names of at least 10 girls  in the chat Instagram And also WhatsApp") 
+elif name== "vinay":
+  print("You are chatting with some one still now\n You are seeing bits daily in bathroom\n")
+
 elif name == "ammu":
   print("Your name is boochi[ psycho pilla ]\n I am very interested to do patches of my friends \nI am walking like rabbit\nI always eat carrets\nNaku koncham thikka undi danikoka lekkundi")
 elif name == "venkatesh":
   print("you are fall in love in recently with pace college girl \n now your are studying in qis college in branch of cse-6\n pedha kutha lodivi \n ")
 elif name == "yeshu1":
   print("your name is pikachu [ mental pilla ]\n Daily the patch is done by boochi \n Daily carrying a large carriage box that can eat easily 10 members \n Naku koncham thikka undhi dhaniki asalu lekkee ledhu ")
-elif name== "vinay":
-  Print("You are chatting with some one still now\n You are seeing bits daily in bathroom\n")
 
    
 
 
-elif name == "name":
-   Print("hello")
-elif name == "name":
-   Print("hello")
-elif name == "name":
-   Print("hello")
 
-elif name == "name":
-   Print("hello")
-
-elif name == "name":
-   Print("hello")
-
-elif name == "name":
-   Print("hello")
